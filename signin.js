964a5352526f2b933bb836d843fd97adc5344e59
dpizzle38 <130174020+dpizzle38@users.noreply.github.com>
2023-06-17 12:53:43 -0700

    fixed the issues with the sign in

--- signin.js
+++ signin.js
@@ -1,57 +1,64 @@
 const signIn = document.getElementById("signIn");
 signIn.innerText = "Sign in";
-signIn.addEventListener('click',(event) =>{
-    event.preventdefault();
+
+signIn.addEventListener("click", (event) => {
+    event.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    signIn.innerText = "loading....";
-    signIn.classList.add(pulse);
+    signIn.innerText = "loading...";
+    signIn.classList.add("pulse");
 
     if(
-        email ===""||
+        email === "" ||
         password === ""
     ){
         swal.fire({
             icon : "info",
             text: "All fields required",
             confirmButtonText : "ok"
-        })
+        });
         signIn.innerText = "Sign in";
         signIn.classList.remove("pulse");
     } else {
         const signInData = new FormData();
         signInData.append("email", email);
-        signInData.apppend("password", password);
+        signInData.append("password", password);
 
         const signReq = {
             method : "POST",
             body: signInData,
-        }
+        };
         
-        const URL = "https://pluralcodesandbox.com/yorubalearning/api/admin.login";
+        const URL = "https://pluralcodesandbox.com/yorubalearning/api/admin_login";
         
         fetch (URL, signReq)
-        .then((response) => response.json())
-        .then((result) =>{
-          localStorage.setItem("adminObj", JSON.stringify(result));  
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result, result.status);
+                localStorage.setItem("adminObj", JSON.stringify(result));  
 
-          const getAdminObj = localStorage.getItem("adminObj");
-          const adminObj = Json.parse(getAdminObj);
+                const getAdminObj = localStorage.getItem("adminObj");
+                const adminObj = JSON.parse(getAdminObj);
 
-          if(adminObj.hasOwnProperty("email")){
-            location.href ="dashboard.html"
-          }else{
-            swal.fire({
-                icon : "warning",
-                text: "Login unsuccesful",
-                confirmButtonColor : "#2D85DE",
-            })
-            signIn.innerText = "sign In"
-            signIn.classList.remove("pulse");
-          }
+                if (adminObj.hasOwnProperty("email")) {
+                    location.href ="dashboard.html";
+                } else {
+                    swal.fire({
+                        icon : "warning",
+                        text: "Login unsuccesful",
+                        confirmButtonColor : "#2D85DE",
+                    });
+
+                }
+                signIn.innerText = "sign In"
+                signIn.classList.remove("pulse");
+                
+          
         })
-        .catch((error) => console.log("Error", error));
+        .catch((error) => {
+            console.log("Error", error);
+        });
     }
     
 })
